6bba8cf50c5c0d856946cef94f9c6b46a3bb1a91
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-03-09 13:07:07 +0100

    add categories.html and add_category.html and get/post methods in routes.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,5 +10,6 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+app.app_context().push()
 
 from taskmanager import routes  # noqa
